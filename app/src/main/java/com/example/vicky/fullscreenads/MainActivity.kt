ac49bba5f9fce581a02babeaa202ba38e1cc665f
kmvignesh <kmvignesh94@gmail.com>
2018-11-25 21:54:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vicky/fullscreenads/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.vicky.fullscreenads
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mInterstitialAd: InterstitialAd
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this,getString(R.string.admob_app_id))
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId= getString(R.string.interstital_ad_id)
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+//        mInterstitialAd.adListener = object : AdListener(){
+//
+//            override fun onAdLoaded() {
+//                mInterstitialAd.show()
+//                super.onAdLoaded()
+//            }
+//
+//        }
+
+        btn_load_ad.setOnClickListener {
+            if(mInterstitialAd.isLoaded)
+                mInterstitialAd.show()
+        }
+
+    }
+}
